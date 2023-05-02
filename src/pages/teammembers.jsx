c5b97fd0a512e2f87f2a58da23006c853be68abd
dubeyom <omdubey9977@gmail.com>
2023-05-02 21:33:15 +0530

    ui

--- src/pages/teammembers.jsx
+++ src/pages/teammembers.jsx
@@ -38,72 +38,66 @@ function TeamMembers() {
   
 
     <Layout>
-        <div className=' flex flex-col md:pl-2 lg:pl-40 xl:pl-60 ' >
-
-        <div className="flex justify-between gap-2 items-center    px-48">
-           
-           <div className='flex flex-row justify-between py-3'>
-           <div className='flex flex-row justify-between gap-2 ' >
-           <input
-               type="text"
+    <div className="flex flex-col md:pl-2 lg:pl-40 xl:pl-60">
+  
+      <div className="flex justify-between items-center px-4 md:px-10 lg:px-20 py-3">
+        <div className="flex flex-row justify-between gap-2 w-full md:w-auto">
+          <input
+            type="text"
             placeholder="Search..."
-                   className=" px-4 py-2 w-96 outline-1 bg-green-200 text-black focus:outline-black rounded-md tracking-wide"
-              />
-           <div className=" bg-red-400 p-4  rounded-full cursor-pointer">
-              <FaSearch className="text-white " />
-              </div>
-           </div>
-            </div>
-            <button onClick={() => setShowMyModal(true)} className='p-2 bg-red-400 rounded-md cursor-pointer hover:ease-in text-white'>Add Members</button>
-           </div>
-           
-
-                       {/* {table } */}
-                       <div className="bg-red-200 p-4 px-20 text-center flex flex-col gap-10 ">
-          <table>
-            <thead>
-              <tr>
-                <th>S.No</th>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Flat number</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((row) => (
-                <tr key={row.sNo}>
-                  <td>{row.sNo}</td>
-                  <td>{row.name}</td>
-                  <td>{row.serviceName}</td>
-                  <td>{row.date}</td>
-                  <td className='flex justify-evenly'>
-                    <FaTrashAlt
-                      className="hover:text-red-500 hover:ease-out  rounded-md cursor-pointer  "
-                      // onClick={() => handleDeleteClick(row)}
-                    />
-                   
-                    <span> / </span>
-                    
-                    <FaEdit
-                      className="hover:text-blue-500 hover:ease-out rounded-md cursor-pointer"
-                      onClick={() => handleEditClick(row)}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-
-
-
-                       
-             </div>
-             <Modal onClose={handekOnClose} visible={showMyModal}/>
-
+            className="px-4 py-2 w-full md:w-96 outline-1 bg-green-200 text-black focus:outline-black rounded-md tracking-wide"
+          />
+          <div className="bg-red-400 p-4 rounded-full cursor-pointer">
+            <FaSearch className="text-white" />
+          </div>
         </div>
-         </Layout>
+        <button
+          onClick={() => setShowMyModal(true)}
+          className="p-2 bg-red-400 rounded-md cursor-pointer hover:ease-in text-white"
+        >
+          Add Members
+        </button>
+      </div>
+  
+      {/* {table } */}
+      <div className="bg-red-200 p-4 px-10 md:px-20 text-center flex flex-col gap-10">
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">S.No</th>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Position</th>
+              <th className="px-4 py-2">Flat number</th>
+              <th className="px-4 py-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row) => (
+              <tr key={row.sNo}>
+                <td className="px-4 py-2">{row.sNo}</td>
+                <td className="px-4 py-2">{row.name}</td>
+                <td className="px-4 py-2">{row.serviceName}</td>
+                <td className="px-4 py-2">{row.date}</td>
+                <td className="flex justify-evenly">
+                  <FaTrashAlt
+                    className="hover:text-red-500 hover:ease-out rounded-md cursor-pointer"
+                    // onClick={() => handleDeleteClick(row)}
+                  />
+                  <span>/</span>
+                  <FaEdit
+                    className="hover:text-blue-500 hover:ease-out rounded-md cursor-pointer"
+                    onClick={() => handleEditClick(row)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Modal onClose={handekOnClose} visible={showMyModal} />
+    </div>
+  </Layout>
+  
   )
 }
 
