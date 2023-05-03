ee093d1b3b8a141618506324c5fc7b93b00385e9
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-05-03 14:25:39 +0530

    added supabse for media storage

---
+++ src/components/ImageUploader.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react';
+import { supabase } from '../utils/supabaseClient';
+
+function ImageUploader() {
+    const [file, setFile] = useState(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (!file) return
+
+        const path = `${file.name}`
+        const { data, error } = await supabase.storage.from('property_images').upload(path, file)
+
+        if (error) {
+            console.error(error)
+        } else {
+            console.log('File uploaded successfully:', data)
+            const { data: url, error: err } = supabase.storage.from('gallery').getPublicUrl(path)
+            console.log(url)
+        }
+    }
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0])
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleFileChange} />
+                <button type="submit">Upload</button>
+            </form>
+        </div>
+    )
+}
+
+export default ImageUploader
