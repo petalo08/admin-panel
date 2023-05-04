c20dc7eb102de68852b322c96693749a62d8349d
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-05-04 20:00:46 +0530

    added admin page

---
+++ src/pages/admin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import AdminUsersTable from '../components/pages/admin/AdminUsersTable'
+
+function admin() {
+    return (
+        <Layout>
+            <div></div>
+            <AdminUsersTable />
+        </Layout>
+    )
+}
+
+export default admin
