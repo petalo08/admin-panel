0a997d65e76b11151dcffe86af593452bd32f19a
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-05-04 19:45:37 +0530

    added update description successfully

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout"
 import Image from 'next/image';
 import { FiSearch } from 'react-icons/fi';
 import GalleryModal from "../components/GalleryModal"
-import { getGalleryById } from "../api/gallery";
+import { getGalleryById, updateGalleryById } from "../api/gallery";
 
 
 // const images = [
@@ -42,64 +42,81 @@ function Gallery() {
       console.log(err)
     }
   }
+  const handleUpdateGallery = async () => {
+    try {
+      let body = {
+        description: description
+      }
+      const res = await updateGalleryById(body)
+      if (res.status === 200) {
+        alert("Updated successfully")
+      }
+    }
+    catch (err) {
+      console.error(err)
+    }
+    useEffect(() => {
+      handleFetchGalleryData()
+    }, [])
 
-  useEffect(() => {
-    handleFetchGalleryData()
-  }, [])
-
-  return (
-    <Layout>
-      <div className="flex flex-col md:pl-2 lg:pl-40 xl:pl-60">
-        <div className="container mx-auto px-4 lg:px-8 py-16">
-          <div className="flex justify-between items-center py-4 gap-2">
-            <div className="flex items-center ">
-              <input
-                type="text"
-                placeholder="Search images"
-                className="px-3 py-2 text-gray-700 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 transition-colors w-96 bg-gray-100"
-              />
-              <button className="ml-2 p-2 bg-red-300 cursor-pointer rounded-md text-white">
-                <FiSearch className="h-6 w-6" />
+    return (
+      <Layout>
+        <div className="flex flex-col md:pl-2 lg:pl-40 xl:pl-60">
+          <div className="container mx-auto px-4 lg:px-8 py-16">
+            <div className="flex justify-between items-center py-4 gap-2">
+              <div className="flex items-center ">
+                <input
+                  type="text"
+                  placeholder="Search images"
+                  className="px-3 py-2 text-gray-700 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 transition-colors w-96 bg-gray-100"
+                />
+                <button className="ml-2 p-2 bg-red-300 cursor-pointer rounded-md text-white">
+                  <FiSearch className="h-6 w-6" />
+                </button>
+              </div>
+              <button
+                onClick={() => setShowMyModal(true)}
+                className="p-2 bg-red-300 cursor-pointer rounded-md text-white">
+                Upload Image
               </button>
             </div>
-            <button
-              onClick={() => setShowMyModal(true)}
-              className="p-2 bg-red-300 cursor-pointer rounded-md text-white">
-              Upload Image
-            </button>
-          </div>
-          {/* {description } */}
-          <div className="container mx-auto px-4 lg:px-8 py-16 flex flex-row justify-between items-center">
-            <input
-              type="text"
-              placeholder="Add description"
-              value={description}
-              onChange={handleOnChange}
-              className="w-full py-2 px-4 h-20 text-gray-700 bg-gray-100 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 transition-colors"
-            />
-            <div className="w-full md:w-1/3 lg:w-1/2 xl:w-1/3 flex justify-center md:justify-end">
-              <button className="px-4 py-2 h-14  bg-red-300 text-white rounded-md">Update Description</button>
+            {/* {description } */}
+            <div className="container mx-auto px-4 lg:px-8 py-16 flex flex-row justify-between items-center">
+              <input
+                type="text"
+                placeholder="Add description"
+                value={description}
+                onChange={handleOnChange}
+                className="w-full py-2 px-4 h-20 text-gray-700 bg-gray-100 rounded-md focus:outline-none focus:ring focus:ring-indigo-200 transition-colors"
+              />
+              <div className="w-full md:w-1/3 lg:w-1/2 xl:w-1/3 flex justify-center md:justify-end">
+                <button
+                  onClick={() => handleUpdateGallery()}
+                  className="px-4 py-2 h-14  bg-red-300 text-white rounded-md">
+                  Update Description
+                </button>
+              </div>
+            </div>
+            {/* { Gallery images} */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className="relative overflow-hidden transform transition-all ease-out duration-500 hover:scale-105 rounded-md"
+                >
+                  <Image src={image?.url} alt={image?.alt} width={800} height={800} />
+                </div>
+              ))}
             </div>
           </div>
-          {/* { Gallery images} */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-            {images.map((image, index) => (
-              <div
-                key={index}
-                className="relative overflow-hidden transform transition-all ease-out duration-500 hover:scale-105 rounded-md"
-              >
-                <Image src={image?.url} alt={image?.alt} width={800} height={800} />
-              </div>
-            ))}
-          </div>
+
+          <GalleryModal onClose={handleOnClose} visible={showMyModal} />
+
         </div>
+      </Layout>
 
-        <GalleryModal onClose={handleOnClose} visible={showMyModal} />
-
-      </div>
-    </Layout>
-
-  )
+    )
+  }
 }
 
 export default Gallery
