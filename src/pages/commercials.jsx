487ea424b989b95f9cd83530afd4a0209ce2581e
dubeyom <omdubey9977@gmail.com>
2023-05-05 16:08:25 +0530

    ui

--- src/pages/commercials.jsx
+++ src/pages/commercials.jsx
@@ -7,3 +7,31 @@ function commercials() {
 }
 
 export default commercials
+
+export async function getServerSideProps(ctx) {
+  const { req, res } = ctx;
+  const token = req.cookies.authToken;
+  const user = req.cookies.user 
+  
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  // if(user.role == "user")
+  // {
+    
+  //   return {
+  //     redirect: {
+  //       destination: "/",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
+  return {
+    props: {},
+  };
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import Login from '../components/Login'
+import React from "react";
+import Login from "../components/Login";
 
 function login() {
-    return (
-        <Login />
-    )
+  return <Login />;
 }
 
-export default login
+export default login;
+
+export async function getServerSideProps(ctx) {
+  const { req, res } = ctx;
+  const token = req.cookies.authToken;
+  if (token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
