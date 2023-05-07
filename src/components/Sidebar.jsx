7894dc91c2829fced916429fdcfe8e8e98ce0938
dubeyom <omdubey9977@gmail.com>
2023-05-07 22:51:10 +0530

    scroll-added-sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -169,45 +169,59 @@ const Sidebar = () => {
 
   return (
     <div className="sidebar__wrapper">
-      <button className="collapsed__btn" onClick={toggleSidebarcollapse}>
-        {isCollapsed ? <MdKeyboardArrowRight /> : <MdKeyboardArrowLeft />}
-      </button>
-      <Box
-        position='sticky'
-        top='0'
-        left='0'
-        w={isCollapsed ? "max-content" : ["max-content", "max-content", "15vw"]}
-        h='100%'
-        as='aside'
-        className="sidebar" data-collapse={isCollapsed}>
-        <Box as='div'
-          w={"100%"}
-          mb={12}
-          pb={6}
-          borderBottom={isCollapsed ? "none" : "1px solid #e5e5e5"}
-          textAlign={'center'}>
-          {isCollapsed ? ("OP") : ("Orchid Petal")}
-        </Box>
-        <ul className="sidebar__list">
-          {sidebarItems.map(({ name, href, icon: Icon }) => {
-            return (
-              <li className="sidebar__item" key={name}>
-                <Link
-                  className={`sidebar__link ${router.pathname === href ? "sidebar__link--active" : ""
-                    }`}
-                  href={href}
-                >
-                  <span className="sidebar__icon">
-                    <Icon />
-                  </span>
-                  <span className="sidebar__name">{name}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </Box>
-    </div>
+  <button className="collapsed__btn" onClick={toggleSidebarcollapse}>
+    {isCollapsed ? <MdKeyboardArrowRight /> : <MdKeyboardArrowLeft />}
+  </button>
+  <Box
+    position="sticky"
+    top="0"
+    left="0"
+    w={isCollapsed ? "max-content" : ["max-content", "max-content", "15vw"]}
+    h="100%"
+    as="aside"
+    className="sidebar"
+    data-collapse={isCollapsed}
+    sx={{
+      overflowY: "scroll",
+      scrollbarWidth: "none",
+      msOverflowStyle: "none",
+      "&::-webkit-scrollbar": {
+        width: "0px",
+        background: "transparent",
+      },
+    }}
+  >
+    <Box
+      as="div"
+      w={"100%"}
+      mb={12}
+      pb={6}
+      borderBottom={isCollapsed ? "none" : "1px solid #e5e5e5"}
+      textAlign={"center"}
+    >
+      {isCollapsed ? "OP" : "Orchid Petal"}
+    </Box>
+    <ul className="sidebar__list">
+      {sidebarItems.map(({ name, href, icon: Icon }) => {
+        return (
+          <li className="sidebar__item" key={name}>
+            <Link
+              className={`sidebar__link ${
+                router.pathname === href ? "sidebar__link--active" : ""
+              }`}
+              href={href}
+            >
+              <span className="sidebar__icon">
+                <Icon />
+              </span>
+              <span className="sidebar__name">{name}</span>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  </Box>
+</div>
   );
 };
 
