7f4c925316dc9f748282a150912b861ff4040b79
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-05-30 20:32:19 +0530

    fixed team member login issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -114,27 +114,27 @@ const SidebarCards = () => {
 
 export default SidebarCards;
 
-// export async function getServerSideProps(ctx) {
-//   const { req, res } = ctx;
-//   const token = req.cookies.authToken;
-//   if (!token) {
-//     return {
-//       redirect: {
-//         destination: "/login",
-//         permanent: false,
-//       },
-//     };
-//   }
-//   const user = JSON.parse(req.cookies.user);
-//   if (user.role === "user") {
-//     return {
-//       redirect: {
-//         destination: `/`,
-//         permanent: false,
-//       },
-//     };
-//   }
-//   return {
-//     props: {},
-//   };
-// }
+export async function getServerSideProps(ctx) {
+  const { req, res } = ctx;
+  const token = req.cookies.authToken;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  // const user = JSON.parse(req.cookies.user);
+  // if (user.role === "user") {
+  //   return {
+  //     redirect: {
+  //       destination: `/`,
+  //       permanent: false,
+  //     },
+  //   };
+  // }
+  return {
+    props: {},
+  };
+}
