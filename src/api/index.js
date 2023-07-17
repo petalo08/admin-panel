187c86363e6b1edccd9e31aec2768a933a3e568b
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-07-17 16:28:47 +0530

    admin panel api fixed

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const prefix = "/api/v1";
-export const baseurl = "https://backend-delta-liart-17.vercel.app";
+export const baseurl = "http://localhost:5002";
+// export const baseurl = "https://backend-delta-liart-17.vercel.app";
 export const url = baseurl + prefix;
 const API_INSTANCE = axios.create({ baseURL: url });
 
--- src/components/pages/teamMembers/EditTeamMemberModal.jsx
+++ src/components/pages/teamMembers/EditTeamMemberModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Modal,
   ModalOverlay,
@@ -8,11 +8,103 @@ import {
   ModalBody,
   ModalCloseButton,
   Button,
+  useToast,
+  Stack,
 } from '@chakra-ui/react'
+import { updateTeamMemberById } from '../../../api/teamMember'
+import { supabase } from '../../../utils/supabaseClient'
 
 const EditTeamMemberModal = (props) => {
+  const toast = useToast()
   const { isOpen, onOpen, onClose, data } = props
   console.log(data)
+  const [newData, setNewData] = useState({})
+  const [image, setImage] = useState(null)
+  const [fl, setFl] = useState(null)
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    const allowedExtensions = /(\.jpg|\.jpeg|\.png|\.webp|\.svg)$/i;
+
+    if (!allowedExtensions.exec(file.name)) {
+      alert("Error: Please choose a file with a valid image format (jpg, jpeg, png, webp, svg).");
+      event.target.value = "";
+      return;
+    }
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    setFl(file)
+  }
+
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+    console.log(e.target.name.value)
+    console.log('doing')
+    if (fl) {
+      const path = `members/${fl.name}`
+      const { data: data2, error } = await supabase.storage.from('images').upload(path, fl)
+      if (error) {
+        console.error(error)
+      } else {
+        console.log('File uploaded successfully:', data)
+        const { data: url, error: err } = supabase.storage.from('images').getPublicUrl(path)
+        if (err) {
+          console.error(err)
+          return
+        }
+        setImage(null)
+        setFl(null)
+        const res = await updateTeamMemberById(data?._id, {
+          name: e.target.name.value,
+          designation: e.target.designation.value,
+          image: url.publicUrl || data.image,
+        })
+        if (res.status === 200) {
+          console.log(res.data)
+          toast({
+            title: "Details uploaded successfully",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+          })
+        }
+      }
+    }
+    else {
+      try {
+        const res = await updateTeamMemberById(data?._id, {
+          name: e.target.name.value,
+          designation: e.target.designation.value,
+        })
+        if (res.status === 200) {
+          console.log(res.data)
+          toast({
+            title: "Details uploaded successfully",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+          })
+        }
+      }
+      catch (err) {
+        console.log(err)
+        toast({
+          title: "Error",
+          description: "Something went wrong",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        })
+      }
+    }
+  }
+  useEffect(() => {
+    setNewData(data)
+  }, [])
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -21,7 +113,7 @@ const EditTeamMemberModal = (props) => {
           <ModalHeader>Edit details</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <form>
+            <form onSubmit={handleUpdate}>
               <div className="flex flex-col gap-5">
                 <div className="flex flex-col gap-2">
                   <label htmlFor="name">Name</label>
@@ -29,8 +121,8 @@ const EditTeamMemberModal = (props) => {
                     type="text"
                     name="name"
                     id="name"
-                    value={data.name}
-                    onChange={(e) => setData({ ...data, name: e.target.value })}
+                    defaultValue={data.name}
+                    onChange={(e) => setNewData({ ...data, name: e.target.value })}
                     className="border border-gray-700 p-2 rounded"
                   />
                 </div>
@@ -40,9 +132,9 @@ const EditTeamMemberModal = (props) => {
                     type="text"
                     name="designation"
                     id="designation"
-                    value={data.designation}
+                    defaultValue={data.designation}
                     onChange={(e) =>
-                      setData({ ...data, designation: e.target.value })
+                      setNewData({ ...data, designation: e.target.value })
                     }
                     className="border border-gray-700 p-2 rounded"
                   />
@@ -53,14 +145,21 @@ const EditTeamMemberModal = (props) => {
                     type="file"
                     name="image"
                     id="image"
-                    
-                    
-                    onChange={(e) => setImage(e.target.files[0])}
+                    accept=".jpg, .jpeg, .png, .webp, .svg"
+                    onChange={handleImageUpload}
                     className="border border-gray-700 p-2 rounded"
                   />
-                  <img src={data.image} alt={data.name} />
+                  {image ? (
+                    <img src={image} alt="preview" className="w-24 h-24" />)
+                    :
+                    (< img src={data.image} alt={data.name} className="w-24 h-24" />)}
                 </div>
               </div>
+              <Stack justify='center' my={2} align='center'>
+                <Button type='submit' colorScheme='blue'>
+                  Update
+                </Button>
+              </Stack>
             </form>
           </ModalBody>
         </ModalContent>
